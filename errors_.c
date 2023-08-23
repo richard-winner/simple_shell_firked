28b8082da30a10e1bb80130941009a0a267adb5c
dr-winner <duvorrichardwinner@gmail.com>
2023-08-23 05:56:54 -0700

    my first commit on 23/08/2023

---
+++ errors_.c
@@ -0,0 +1,79 @@
+#include "shell.h"
+/**
+ * _eputs - this function prints an input string
+ * @str: this is the string to be printed
+ * Return: nay
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+/**
+ * _eputchar - function writes the character c to stderr
+ * @c: character to be printed
+ * Return: On success return 1, else
+ * return -1 and errno is set appropriately.
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - the function here writes the character c to given fd
+ * @c: this is the character to be printed
+ * @fd: this is the filedescriptor to write to
+ * Return: On success, return 1,
+ * error, -1 is returned, and errno is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putsfd - this function here prints an input string
+ * @str: this is the string to be printed
+ * @fd: this is the filedescriptor to write to
+ * Return: here is the number of chars put
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
